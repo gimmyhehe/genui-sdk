c03cd5c31de4a8ab4f9e1ae2465fdc17c59f44f4
cenguanming <975402925@qq.com>
2026-06-27 16:51:34 +0800

    fix: restore vue change

--- packages/frameworks/vue/src/index.ts
+++ packages/frameworks/vue/src/index.ts
@@ -1,9 +1,4 @@
 export * from './chat';
 export * from './renderer';
 export * from './config-provider';
-export { RENDERER_SETTINGS } from '@opentiny/tiny-schema-renderer';
-/**
- * @deprecated use RENDERER_SETTINGS instead
- */
-export { RENDERER_SETTINGS as RENDERER_SETTINGS_KEY } from '@opentiny/tiny-schema-renderer';
-
+export { RENDERER_SETTINGS_KEY } from '@opentiny/tiny-schema-renderer';
