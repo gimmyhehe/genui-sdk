2f8425c5802051ade2a7d5026d59492d0fcb0733
cenguanming <975402925@qq.com>
2026-07-16 16:03:38 +0800

    fix(renderer): update to GenuiRenderer and inject materials configuration

--- packages/frameworks/angular/projects/renderer-element/src/main.ts
+++ packages/frameworks/angular/projects/renderer-element/src/main.ts
@@ -2,16 +2,18 @@ import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { GenuiLegacyRenderer } from '@opentiny/genui-sdk-angular';
+import { GenuiRenderer, GENUI_MATERIALS } from '@opentiny/genui-sdk-angular';
+import { materials } from '@opentiny/genui-sdk-materials-angular-opentiny-ng/materials';
 
 const ELEMENT_TAG = 'genui-renderer-ng-element';
 createApplication({
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
+    { provide: GENUI_MATERIALS, useValue: materials },
   ]
 }).then((appRef) => {
-  const elementCtor = createCustomElement(GenuiLegacyRenderer, {
+  const elementCtor = createCustomElement(GenuiRenderer, {
     injector: appRef.injector,
   });
   if (!customElements.get(ELEMENT_TAG)) {
